a548931cb915c24b9157332b4235da614bee3a49
virgilvox <moheeb.zara@citrix.com>
2015-11-03 17:46:29 -0700

    added meshblu control - just pass ?uuid=xxxxx&token=xxxxx

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@ var app = angular.module('MyApp');
 
 app.controller('AppCtrl', function($scope) {
 
-  console.log('helllo');
-
   var state = 0;
   $scope.borderColor = "01a982";
   $scope.availText = "Available for Collaboration"
-  
+  $scope.names = "";
+
   $scope.available = function(){
     if(state == 1){
       $scope.borderColor = "01a982";
@@ -21,5 +20,63 @@ app.controller('AppCtrl', function($scope) {
     }
 
   }
+
+  $scope.payload = function(data){
+    $scope.availText = data.payload.text;
+    $scope.borderColor = data.payload.color;
+    $scope.names = data.payload.names;
+    $scope.$apply()
+  }
+
+
+  var GET = {};
+  var query = window.location.search.substring(1).split("&");
+  for (var i = 0, max = query.length; i < max; i++)
+  {
+    if (query[i] === "") // check for trailing & with no param
+    continue;
+    var param = query[i].split("=");
+    GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
+  }
+
+  var conn = meshblu.createConnection({
+    "uuid": GET.uuid,
+    "token": GET.token
+  });
+
+  var MESSAGE_SCHEMA = {
+    "type": 'object',
+    "properties": {
+      "text": {
+        "type": "string",
+        "enum": ["Available for Collaboration", "Collaboration in Progress"]
+      },
+      "color": {
+        "type": "string",
+        "enum": ["01a982", "FF8D6D"]
+      },
+      "names": {
+        "type": "string"
+      }
+    }
+  };
+
+  conn.on('ready', function(data){
+    console.log('UUID AUTHENTICATED!');
+    console.log(data);
+    conn.update({
+      "uuid": GET.uuid,
+      "messageSchema": MESSAGE_SCHEMA
+    });
+
+    conn.on('message', function(data){
+      $scope.payload(data);
+    });
+
+  });
+
+
+
+
 
 });

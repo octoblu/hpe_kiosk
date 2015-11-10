8dcc1d55d82831a615429c0288ef01e40ae637c2
virgilvox <moheeb.zara@citrix.com>
2015-11-10 16:28:59 -0700

    added greeting page

---
+++ js/greeting_app.js
@@ -0,0 +1,72 @@
+
+var app = angular.module('MyApp', ['AutoFontSize']);
+
+app.controller('AppCtrl', function($scope) {
+  $scope.names = "";
+
+
+  $scope.payload = function(data){
+
+  var names = [];
+    data.payload.names.forEach(function(entry) {
+      var greet = "Good ";
+      date = new Date();
+      thehour = date.getUTCHours();
+      if (thehour > 18) { greet += "evening"; }
+      else if (thehour > 12) { greet += "afternoon"; }
+      else { greet += "morning"; }
+      var text = greet + " " + entry.name;
+      names.push(text);
+    });
+
+    $scope.names = names;
+    $scope.$apply()
+  }
+
+
+  var GET = {};
+  var query = window.location.search.substring(1).split("&");
+  for (var i = 0, max = query.length; i < max; i++)
+  {
+    if (query[i] === "")
+    continue;
+    var param = query[i].split("=");
+    GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
+  }
+
+  var conn = meshblu.createConnection({
+    "uuid": GET.uuid,
+    "token": GET.token
+  });
+
+  var MESSAGE_SCHEMA = {
+    "type": 'object',
+    "properties": {
+      "names": {
+        "type": "array",
+        "maxItems": 3,
+        "items": {
+        "type": "object",
+        "properties": {
+          "name": { "type": "string" }
+        }
+      }
+      }
+    }
+  };
+
+  conn.on('ready', function(data){
+    console.log('UUID AUTHENTICATED!');
+    console.log(data);
+    conn.update({
+      "uuid": GET.uuid,
+      "messageSchema": MESSAGE_SCHEMA
+    });
+
+    conn.on('message', function(data){
+      $scope.payload(data);
+    });
+
+  });
+
+});
